a55f04e3ba685ee66b73a6be118934231962e94b
Puneet Arora <arorapuneet234@gmail.com>
2025-11-19 16:15:57 +0530

    Created function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+
+
+function getComputerChoice(){
+    let choice=Math.floor(Math.random()*12)+1;
+    
+    if(choice>=1&&choice<=4)return "Rock"
+    else if(choice>=5&&choice<=8)return "Paper"
+    else return "Scissors" 
+}
+
+console.log(getComputerChoice());
